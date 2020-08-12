0a518b93ce2b0d9991eba623086e3500007c18d1
Vitor <Vitor>
2020-08-12 23:05:31 +0100

    Added drawer

--- routes/aboutStack.js
+++ routes/aboutStack.js
@@ -1,33 +1,26 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 
-import Home from '../screens/home';
-import ProductDetails from '../screens/productDetails';
+import About from '../screens/about';
 import Header from '../shared/header';
 import React from 'react';
 
 const screens = {
-  Home: {
-    screen: Home,
+  About: {
+    screen: About,
     navigationOptions: ({ navigation }) => {
       return {
-        headerTitle: () => <Header title='home ' navigation={navigation} />
+        headerTitle: () => <Header title='about' navigation={navigation} />
       }
     }
-  },
-  ProductDetails: {
-    screen: ProductDetails,
-    navigationOptions: {
-      title: 'Product Details'
-    }
-  },
+  }
 };
 
-const HomeStack = createStackNavigator(screens, {
+const AboutStack = createStackNavigator(screens, {
   defaultNavigationOptions: {
     headerTintColor: '#444',
     headerStyle: { backgroundColor: '#eee', height: 80 }
   }
 });
 
-export default HomeStack;
+export default AboutStack;
