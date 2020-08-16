b47233168c864ad4ae87f4ba11010b9d91cebb31
Vitor <Vitor>
2020-08-16 12:45:25 +0100

    fix build

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
+import { registerRootComponent } from 'expo';
 
-AppRegistry.registerComponent(appName, () => gestureHandlerRootHOC(App));
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in the Expo client or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
